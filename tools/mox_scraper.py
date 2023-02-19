cfc578e7b1a08626e202036e5e8da9fdcd320c88
Andrew <andrewknoesen@gmail.com>
2023-02-19 16:23:56 +0200

    FIXED: Remove partial matches

--- tools/mox_scraper.py
+++ tools/mox_scraper.py
@@ -1,6 +1,9 @@
+import json
+import sys
 import pandas as pd
 from pprint import pprint
 import requests
+
 
 def get_id(exact_card: str):
     # https://mox.rouxtaccess.com/api/card/search?name=Evolved%20Sleeper&uniqueName=1
@@ -12,9 +15,9 @@ def get_id(exact_card: str):
 
     mox_response = requests.get(
         url="https://mox.rouxtaccess.com/api/card/search", params=search_parameters)
-        
+
     data = mox_response.json()
-    
+
     print("###################################")
     print(f"Returned retailer Raw for card {exact_card}:")
     print("###################################")
@@ -22,66 +25,76 @@ def get_id(exact_card: str):
     print("###################################")
     print("###################################")
 
-
-    
-
     # print(data['data'])
     return int(data['data'][0]['id'])
 
+
 def get_retailers(id: int, name: str):
-    retailers = requests.post(
-        f"https://mox.rouxtaccess.com/api/card/{id}/scrape/queued?include=retailer")
+    endpoint: str = f"https://mox.rouxtaccess.com/api/card/{id}/scrape/queued?include=retailer"
+    retailers: requests.Response = requests.post(endpoint)
+    compiled_reatilers: list = retailers.json()['data']
+
+    for i in range(2, retailers.json()['meta']['pagination']['total_pages'] + 1):
+        retailers_page = requests.post(
+            f'https://mox.rouxtaccess.com/api/card/{id}/scrape/queued?include=retailer&page={i}')
+        for item in retailers_page.json()['data']:
+            compiled_reatilers.append(item)
 
     print("###################################")
     print("Returned retailer Raw:")
+    print(f"Endpoint: {endpoint}")
     print("###################################")
-    pprint(retailers.json())
+    pprint(compiled_reatilers)
     print("###################################")
     print("###################################")
     retail = {}
     private = {}
-    for item in retailers.json()['data']:
-
-        if '?' not in item['priceRead']:
-            retail[item['id']] = {
-                'name': name,
-                'name_long': item['name'],
-                'cost': float(item['priceRead'][1:].replace(",", "")),
-                'qty': item['stock'],
-                'supplier': item['retailer']['name'],
-                'link': item['link']
-            }
-        else:
-            private[item['id']] = {
-                'name': name,
-                'name_long': item['name'],
-                'cost': item['priceRead'],
-                'qty': item['stock'],
-                'supplier': item['retailer']['name'],
-                'link': item['link']
-            }
+    for item in compiled_reatilers:
+        print(item)
+        response_card_name: str = item['name']
+        if name in response_card_name.split(' '):
+            if '?' not in item['priceRead']:
+                retail[item['id']] = {
+                    'name': name,
+                    'name_long': item['name'],
+                    'cost': float(item['priceRead'][1:].replace(",", "")),
+                    'qty': item['stock'],
+                    'supplier': item['retailer']['name'],
+                    'link': item['link']
+                }
+            else:
+                private[item['id']] = {
+                    'name': name,
+                    'name_long': item['name'],
+                    'cost': item['priceRead'],
+                    'qty': item['stock'],
+                    'supplier': item['retailer']['name'],
+                    'link': item['link']
+                }
 
     print("###################################")
     print("Returned retailer:")
     print("###################################")
-    print(retail)
+    pprint(retail)
 
     return {
         'retail': retail,
         'private': private
     }
 
+
 def clean_up(cards: list) -> list:
     pass
 
+
 def scrape_mox(cards_df: pd.DataFrame) -> list:
-    
+
     print("###################################")
     print(f'Scraping')
     print(cards_df)
     print("###################################")
     compiled_retailers = []
-    
+
     dict_len = len(cards_df.index)
     print("###################################")
     print(f'Cards loaded: {dict_len}')
@@ -89,16 +102,18 @@ def scrape_mox(cards_df: pd.DataFrame) -> list:
     print("###################################")
     count = 0
     for card in cards_df['Item']:
-        
+
         id = get_id(card)
 
         compiled_retailers.append(get_retailers(id, card)['retail'])
         # compiled_private.append(get_retailers(id, card)['retail'])
-        count+=1
+        count += 1
         print("###################################")
         print(f'Completed: {count}/{dict_len}')
         print("###################################")
 
-
-
     return compiled_retailers
+
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]](sys.argv[2])
